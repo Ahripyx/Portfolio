9cdc6eb76b59e9dc348ab963ca1c237a73fa1b96
Ahri <ahripyx@gmail.com>
2025-12-03 00:52:07 -0500

    Created Synthwave project page with js navigations

---
+++ scripts/projects.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", () => {
+    // Properties
+    const synthwave = document.getElementById("synthwaveButt");
+
+    // Button navigations
+    synthwave.addEventListener("click", () => {
+        window.location.href = "projects/Synthwave.html"
+    })
+})
