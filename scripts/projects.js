c5900ad1f1204b7e656ce990f23695466d39d2fa
Ahri <ahripyx@gmail.com>
2025-12-05 04:01:50 -0500

    Basic page for Lytheria Discord bot project

--- scripts/projects.js
+++ scripts/projects.js
@@ -1,9 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
     // Properties
     const synthwave = document.getElementById("synthwaveButt");
+    const lytheria = document.getElementById("lytheriaButt");
 
     // Button navigations
     synthwave.addEventListener("click", () => {
         window.location.href = "projects/Synthwave.html"
     })
+
+    lytheria.addEventListener("click", () => {
+        window.location.href = "projects/Lytheria.html"
+    })
+
+
 })
