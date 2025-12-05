75694cb714ce0c71603897c9dec4d16fe08475ef
Ahri <ahripyx@gmail.com>
2025-12-05 06:14:29 -0500

    Populated html page navigations for 3 other projects

--- scripts/projects.js
+++ scripts/projects.js
@@ -3,6 +3,9 @@ document.addEventListener("DOMContentLoaded", () => {
     const synthwave = document.getElementById("synthwaveButt");
     const lytheria = document.getElementById("lytheriaButt");
     const abyssal = document.getElementById("abyssalButt");
+    const haver = document.getElementById("haverButt");
+    const calorieTracker = document.getElementById("calorieButt");
+    const questify = document.getElementById("questifyButt");
 
     // Button navigations
     synthwave.addEventListener("click", () => {
@@ -15,6 +18,18 @@ document.addEventListener("DOMContentLoaded", () => {
 
     abyssal.addEventListener("click", () => {
         window.location.href = "projects/abyssal.html"
+    })
+
+    haver.addEventListener("click", () => {
+        window.location.href = "projects/haver.html"
+    })
+
+    calorieTracker.addEventListener("click", () => {
+        window.location.href = "projects/track-a-cal.html"
+    })
+
+    questify.addEventListener("click", () => {
+        window.location.href = "projects/questify.html"
     })
 
 
